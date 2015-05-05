11c66c5a471a2333e1514f56ac9d08671132b279
Markus Wenzel <markus.wenzel@tu-berlin.de>
2015-05-05 15:27:55 +0200

    test with three subjects

--- Matlab/convert/convertEEG.m
+++ Matlab/convert/convertEEG.m
@@ -14,6 +14,9 @@ subdir_list={%'VPpaa_15_03_04'
     'VPpal_15_03_17'    
     };
 
+% TEST WITH THREE SUBJECTS
+subdir_list=subdir_list(1:3,:);
+
 tags={'hf_cr' 'hf_sq' 'lf_cr' 'lf_sq' 'hf_do' 'hf_st'
     'lf_do' 'lf_st' 'hf_es' 'hf_tr' 'lf_es' 'lf_tr'};
 
