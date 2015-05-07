ea7f8153eb28f3f620e4eab8cc7dea3637f84b1b
Patrik Pluchino <patrikpluchino@Patriks-MacBook.local>
2015-05-07 12:46:09 +0200

    ...

---
+++ Matlab/startup/startup_tubbci_Patrizio.m
@@ -0,0 +1,27 @@
+function startup_Patrizio
+
+global BTB
+
+% Path of the data directory for EEG and log files
+DATA_DIR= fullfile('/Users/patrikpluchino/Desktop/', 'data');
+
+% Path of the BBCI public toolbox
+BBCI_DIR= fullfile('/Users/patrikpluchino/Desktop/', 'bbci_public');
+
+% Folder with convert and analysis scripts
+BBCI_PRIVATE_DIR = fullfile('/Users/patrikpluchino/Desktop/', 'CollaborativeStudy');
+
+cd(BBCI_DIR);
+startup_bbci_toolbox('DataDir', DATA_DIR,...
+    'PrivateDir', BBCI_PRIVATE_DIR);
+    
+addpath(genpath(fullfile(BTB.PrivateDir)));
+
+my_dir=fullfile(BTB.PrivateDir,'Matlab','convert');
+addpath(genpath(my_dir));
+cd(my_dir);
+
+
+format compact
+format longg
+end
